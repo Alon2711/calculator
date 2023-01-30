6b06e1190a6dfc5931cc652a6aaa54b6e81bf32d
Alon2711 <107502202+Alon2711@users.noreply.github.com>
2023-01-30 17:17:02 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const a = +prompt ("Введите 1 число");
+const b = +prompt ("Введите 2 число");
+
+const r = a + b;
+const g = a * b;
+const d = a / b;
+const q = a - b;
+
+console.log( a + '+' + b + "=" + r);
+console.log( a + '*' + b + "=" + g);
+console.log( a + '/' + b + "=" + d);
+console.log( a + '-' + b + "=" + q);
